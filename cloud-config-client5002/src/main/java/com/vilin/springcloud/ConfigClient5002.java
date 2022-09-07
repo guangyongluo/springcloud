c6560eab10db572915ad91cb26c96a1376e78688
guangyongluo <guangyongluo@outlook.com>
2022-09-07 19:47:16 +0800

    springcloud(cloud-config-center5001;cloud-config-client5002;cloud-config-client5003 ):spring cloud config and bus.

---
+++ cloud-config-client5002/src/main/java/com/vilin/springcloud/ConfigClient5002.java
@@ -0,0 +1,15 @@
+package com.vilin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient5002 {
+
+  public static void main(String[] args) {
+      SpringApplication.run(ConfigClient5002.class, args);
+  }
+
+}
---
+++ cloud-config-client5003/src/main/java/springcloud/ConfigClient5003.java
@@ -0,0 +1,15 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient5003 {
+
+  public static void main(String[] args) {
+      SpringApplication.run(ConfigClient5003.class, args);
+  }
+
+}
